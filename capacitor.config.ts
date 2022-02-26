9700587f19dd83c56e8ad4906190d9ab4a386d0e
Felipe Chagas Bonadio <febonadio@gmail.com>
2022-02-25 22:22:21 -0300

    Alteracao da home page para iniciar a lista de pokemons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'br.com.bonadio.pokeapi',
   appName: 'pokeApi',
   webDir: 'www',
   bundledWebRuntime: false
